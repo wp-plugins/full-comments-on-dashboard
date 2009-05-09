13d74e5214402572a073f9cb34ed5cf80136a3a2
scribu <scribu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-05-09 10:19:01 +0000

    plugin compatibility
    git-svn-id: https://plugins.svn.wordpress.org/full-comments-on-dashboard/trunk@116699 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- full-comments-on-dashboard.php
+++ full-comments-on-dashboard.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Full Comments On Dashboard
 Description: Show full comments in the Recent Comments box on the admin dashboard.
-Version: 1.0.2
+Version: 1.0.3
 Author: scribu
 Author URI: http://scribu.net/
 Plugin URI: http://scribu.net/wordpress/full-comments-on-dashboard
@@ -30,7 +30,7 @@ function full_comments_on_dashboard($excerpt) {
 	if ( !is_admin() )
 		return $excerpt;
 
-	$content = apply_filters('comment_text', apply_filters('get_comment_text', $comment->comment_content));
+	$content = wpautop($comment->comment_content);
 	$content = substr($content, 3, -5);	// Remove first <p> and last </p>
 	$content = str_replace('<p>', '<p style="display:block; margin:1em 0">', $content);
 
